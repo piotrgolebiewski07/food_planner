6d9a2a9813be8032cae9dd9b1417c163a790f468
Piotr Golebiewski <pgolebiewski07@gmail.com>
2025-12-10 12:45:36 +0100

    Update configuration, environment example and Ingredient model

--- food_planner_app/__init__.py
+++ food_planner_app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
+db = SQLAlchemy(app)
+
 from food_planner_app import ingredients
+from food_planner_app import models
 
