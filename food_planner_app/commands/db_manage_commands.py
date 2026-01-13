de7e9e3dc5823377808953ba535a9e87fe3eb77c
Piotr Golebiewski <pgolebiewski07@gmail.com>
2026-01-13 11:56:57 +0100

    Extended db-manage commands with Recipe sample data

--- food_planner_app/commands/db_manage_commands.py
+++ food_planner_app/commands/db_manage_commands.py
@@ -1,9 +1,16 @@
 import json
 from pathlib import Path
 from food_planner_app import db
-from food_planner_app.models import Ingredient
+from food_planner_app.models import Ingredient, Recipe
 from sqlalchemy import text
 from food_planner_app.commands import db_manage_bp
+
+
+def load_json_data(file_name: str) -> list:
+    json_path = Path(__file__).parent.parent / 'samples' / file_name
+    with open(json_path) as file:
+        data_json = json.load(file)
+        return data_json
 
 @db_manage_bp.cli.group()
 def db_manage():
@@ -15,13 +22,18 @@ def db_manage():
 def add_data():
     """Add sample data to database"""
     try:
-        ingredients_path = Path(__file__).parent.parent / 'samples' / 'ingredients.json'
-        with open(ingredients_path) as file:
-            data_json = json.load(file)
+        data_json = load_json_data('ingredients.json')
         for item in data_json:
             ingredient = Ingredient(**item)
             db.session.add(ingredient)
+
+        data_json = load_json_data('recipes.json')
+        for item in data_json:
+            recipe = Recipe(**item)
+            db.session.add(recipe)
+
         db.session.commit()
+        print("Sample ingredients and recipes added successfully.")
     except Exception as exc:
         print('Unexpected error: {}'.format(exc))
 
@@ -30,9 +42,12 @@ def add_data():
 def remove_data():
     """Remove all data from the database"""
     try:
-        db.session.execute(text('TRUNCATE TABLE ingredients'))
+        db.session.execute(text('DELETE FROM recipes'))
+        db.session.execute(text('ALTER TABLE recipes AUTO_INCREMENT = 1'))
+        db.session.execute(text('DELETE FROM ingredients'))
+        db.session.execute(text('ALTER TABLE ingredients AUTO_INCREMENT = 1'))
         db.session.commit()
-        print('Data has benn successfully remove from database')
+        print('Data has been successfully remove from database')
     except Exception as exc:
         print('Unexpected error: {}'.format(exc))
 
