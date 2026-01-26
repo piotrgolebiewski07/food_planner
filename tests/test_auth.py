ba3b300c6c3141b9aecacba5ccab2fc1b56b549f
Piotr Golebiewski <pgolebiewski07@gmail.com>
2026-01-26 16:40:10 +0100

    Add negative registration validation tests

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,3 +1,6 @@
+import pytest
+
+
 def test_registration(client):
     response = client.post('/api/v1/auth/register',
                            json={
@@ -11,3 +14,24 @@ def test_registration(client):
     assert response.headers['Content-Type'] == 'application/json'
     assert response_data['success'] is True
     assert response_data['token']
+
+
+@pytest.mark.parametrize(
+    'data, missing_field',
+    [
+        ({'username': 'test', 'password': '123456'}, 'email'),
+        ({'username': 'test', 'email': 'test@gmail.com'}, 'password'),
+        ({'password': '123456', 'email': 'test@gmail.com'}, 'username')
+    ]
+)
+def test_registration_invalid_data(client, data, missing_field):
+    response = client.post('/api/v1/auth/register', json=data)
+    response_data = response.get_json()
+    assert response.status_code == 400
+    assert response.headers['Content-Type'] == 'application/json'
+    assert response_data['success'] is False
+    assert 'token' not in response_data
+    assert missing_field in response_data['message']
+    assert 'Missing data for required field.' in response_data['message'][missing_field]
+    assert isinstance(response_data['message'][missing_field], list)
+
