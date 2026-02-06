799289982aa8d8d93553c49a056e3fb3abaed9f3
Piotr Golebiewski <pgolebiewski07@gmail.com>
2026-02-06 21:40:48 +0100

    Add full CRUD recipes API

--- food_planner_app/recipes/recipes.py
+++ food_planner_app/recipes/recipes.py
@@ -1,8 +1,9 @@
+from flask import request, abort
 from flask import jsonify
 from webargs.flaskparser import use_args
 from food_planner_app import db
 from food_planner_app.recipes import recipes_bp
-from food_planner_app.models import Recipe, RecipeSchema, recipe_schema
+from food_planner_app.models import Recipe, RecipeIngredient, Ingredient, RecipeSchema, recipe_schema
 from food_planner_app.utils import validate_json_content_type, get_schema_args, apply_order, apply_filter, get_pagination, token_required
 from sqlalchemy import select
 from sqlalchemy.exc import IntegrityError
@@ -11,39 +12,112 @@ from sqlalchemy.exc import IntegrityError
 @recipes_bp.route('/recipes', methods=['GET'])
 def get_recipes():
     query = select(Recipe)
-    schema_args = get_schema_args(Recipe)
-    query = apply_order(Recipe, query)
-    query = apply_filter(Recipe, query)
     items, pagination = get_pagination(query, 'recipes.get_recipes')
 
-    recipes = RecipeSchema(**schema_args).dump(items)
+    data = []
+
+    for recipe in items:
+        data.append({
+            "id": recipe.id,
+            "name": recipe.name,
+            "description": recipe.description,
+            "servings": recipe.servings,
+            "ingredients": [
+                {
+                    "name": ri.ingredient.name,
+                    "amount": float(ri.amount),
+                    "unit": ri.ingredient.unit,
+                    "calories": float(ri.ingredient.calories),
+                }
+                for ri in recipe.ingredients
+            ]
+        })
 
     return jsonify({
-        'success': True,
-        'data': recipes,
-        'user_id: int on page': len(recipes),
-        'pagination': pagination
+        "success": True,
+        "data": data,
+        "records_on_page": len(data),
+        "pagination": pagination
     })
 
 
 @recipes_bp.route('/recipes/<int:recipe_id>', methods=['GET'])
 def get_recipe(recipe_id: int):
-    recipe = Recipe.query.get_or_404(recipe_id, description=f'Recipe with id {recipe_id} not found')
+    recipe = Recipe.query.get_or_404(recipe_id)
+
+    data = {
+        "id": recipe.id,
+        "name": recipe.name,
+        "description": recipe.description,
+        "servings": recipe.servings,
+        "ingredients": [
+            {
+                "name": ri.ingredient.name,
+                "amount": float(ri.amount),
+                "unit": ri.ingredient.unit,
+                "calories": float(ri.ingredient.calories),
+            }
+            for ri in recipe.ingredients
+        ]
+    }
+
     return jsonify({
-        'success': True,
-        'data': recipe_schema.dump(recipe)
+        "success": True,
+        "data": data
     })
+
 
 
 @recipes_bp.route('/recipes', methods=['POST'])
 @token_required
 @validate_json_content_type
-@use_args(recipe_schema, error_status_code=400)
-def create_recipe(user_id: int, args: dict):
-    recipe = Recipe(**args)
+def create_recipe(user_id: int):
+    data = request.get_json()
+
+    recipe = Recipe(
+        name=data["name"],
+        description=data.get("description"),
+        servings=data.get("servings", 1),
+    )
+    db.session.add(recipe)
+    db.session.flush()
+
+    for ing in data.get("ingredients", []):
+        ingredient = Ingredient.query.filter_by(name=ing["name"]).first()
+        if not ingredient:
+            abort(400, description=f"Ingredient not found: {ing['name']}")
+
+        db.session.add(
+            RecipeIngredient(
+                recipe_id=recipe.id,
+                ingredient_id=ingredient.id,
+                amount=ing["amount"],
+            )
+        )
+
+    db.session.commit()
+
+    return jsonify({
+        "success": True,
+        "data": {
+            "id": recipe.id,
+            "name": recipe.name
+        }
+    }), 201
+
+@recipes_bp.route('/recipes/<int:recipe_id>', methods=['PUT'])
+@token_required
+@validate_json_content_type
+def update_recipe(user_id: int, recipe_id: int):
+    data = request.get_json()
+
+    recipe = Recipe.query.get_or_404(recipe_id)
+
+    for field in ("name", "description", "servings"):
+        if field in data:
+            setattr(recipe, field, data[field])
 
     try:
-        db.session.add(recipe)
         db.session.commit()
     except IntegrityError:
         db.session.rollback()
@@ -54,38 +128,25 @@ def create_recipe(user_id: int, args: dict):
 
     return jsonify({
         "success": True,
-        "data": recipe_schema.dump(recipe)
-    }), 201
-
-
-@recipes_bp.route('/recipes/<int:recipe_id>', methods=['PUT'])
-@token_required
-@validate_json_content_type
-@use_args(RecipeSchema(partial=True), error_status_code=400)
-def update_recipe(user_id: int, args: dict, recipe_id: int):
-    recipe = Recipe.query.get_or_404(recipe_id, description=f'Recipe with id {recipe_id} not found')
-
-    for key, values in args.items():
-        setattr(recipe, key, values)
-
-    db.session.commit()
-
-    return jsonify({
-        'success': True,
-        'data': recipe_schema.dump(recipe)
-    })
+        "data": {
+            "id": recipe.id,
+            "name": recipe.name,
+            "description": recipe.description,
+            "servings": recipe.servings
+        }
+    }), 200
 
 
 @recipes_bp.route('/recipes/<int:recipe_id>', methods=['DELETE'])
 @token_required
 def delete_recipe(user_id: int, recipe_id: int):
-    recipe = Recipe.query.get_or_404(recipe_id, description=f'Recipe with id {recipe_id} not found')
+    recipe = Recipe.query.get_or_404(recipe_id)
 
     db.session.delete(recipe)
     db.session.commit()
 
     return jsonify({
-        'success': True,
-        'message': f'Recipe with id {recipe_id} has been deleted'
-    })
+        "success": True,
+        "message": f"Recipe {recipe_id} deleted"
+    }), 200
 
